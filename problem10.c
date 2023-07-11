dd2128faba798d9919f845a1a98ad7409523f353
Hamza <wachdarorihadchi@protonmail.ch>
2023-07-11 07:13:15 +0100

    solved problem13

--- problem10.c
+++ problem10.c
@@ -5,28 +5,29 @@
 #include <math.h>
 
 bool isPrime(uint32_t n){
-        if(n < 4)
-                return n > 1;
-        if(!((n % 2) && (n % 3)))
-                return false;
+	if(n < 4)
+		return n > 1;
+	if(!((n % 2) && (n % 3)))
+		return false;
 
-        uint32_t sealling = sqrt(n);
+	uint32_t sealling = sqrt(n);
 
-        for(uint32_t i = 5; i <= sealling; i += 6)
-                if(!( n % (i) && n % (i+2) ))
-                        return false;
+	for(uint32_t i = 5; i <= sealling; i += 6)
+		if(!( n % (i) && n % (i+2) ))
+			return false;
 
-        return true;
+	return true;
 }
 
 int main(void){
-        uint32_t number = 2000000;
+	uint32_t number = 2000000;
 	uint64_t sum = 2;
 
-        for(uint32_t i = 3; i < number; i += 2)
-                if(isPrime(i))
-                        sum += i;
+	for(uint32_t i = 3; i < number; i += 2)
+		if(isPrime(i))
+			sum += i;
 
-        printf("the sum of all the primes below %u is %zu\n", number, sum);
-        return EXIT_SUCCESS;
+	printf("the sum of all the primes below %u is %zu\n", number, sum);
+	return EXIT_SUCCESS;
 }
+
