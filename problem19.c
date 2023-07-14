4622d1f9bd9fc10e3644dac0bae94d525a594bac
Hamza <wachdarorihadchi@protonmail.ch>
2023-07-14 23:57:14 +0100

    solved problem 19 & 20

---
+++ problem19.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+uint8_t days_on_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int main(void){
+	uint8_t first_day = 366 % 7;
+	uint8_t crnt_day = first_day;
+	uint16_t num_mounths = 100 * 12;
+	uint16_t sundays_on_fell_month = 0;
+
+	for(uint16_t i = 0; i != num_mounths+ 1; i++){
+		crnt_day = (crnt_day + days_on_month[i % 12]) % 7;
+		
+		if((i % 12) == 1)
+			if(!(i / 12) % 4)
+				crnt_day++;
+
+		if(crnt_day == 6)
+			sundays_on_fell_month++;
+	}
+
+	printf("%u sunday accure on the 20th centry\n", sundays_on_fell_month);
+
+	return EXIT_SUCCESS;
+}
+
---
+++ problem20.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+#define log10_2piover2 0.3990899341790575
+#define log10_e        0.4342944819032518
+
+const char* factorial(uint64_t n){
+	// aproximation for the log10(n!)
+	uint32_t strlen = (n + 0.5f) * log10f(n) - n * log10_e + log10_2piover2;
+	uint32_t result[strlen + 1];
+	
+	result[strlen] = 1;
+	for(uint32_t i = strlen-1; i != -1; i--)
+		result[i] = 0;
+
+	for(uint32_t i = n; i != 1; i--){
+		uint32_t carry = 0;
+		for(uint32_t j = strlen; j != -1; j--){
+			result[j] = result[j] * i + carry;
+			if(result[j] > 9){
+				carry = result[j] / 10;
+				result[j] = result[j] % 10;
+			} else {
+				carry = 0;
+			}
+		}
+		uint32_t k = strlen;
+		while (carry){
+			result[k-1] += carry % 10;
+			carry /= 10;
+			k--;
+		}
+	}
+
+	char* strres = (char*) malloc(strlen+1);
+	for(uint32_t i = 0; i != strlen + 1; i++)
+		strres[i] = result[i] + '0';
+	strres[strlen+1] = '\0';
+
+	return strres;
+}
+
+int main(void){
+	uint32_t num = 100;
+	const char* res = factorial(num);
+	uint32_t reslen = strlen(res) + 1;
+	uint64_t sum = 0;
+	
+	for(uint32_t i = 0; i != reslen; i++)
+		sum += res[i];
+
+	sum -= '0' * (reslen-1);
+
+	printf("the sum of figits of %u! is %zu\n", num, sum);
+
+	free((void*) res);
+
+	return EXIT_SUCCESS;
+}
+
