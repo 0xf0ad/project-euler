2fbec5b34d8ffa9dea62980c3f4edbbcb56dd776
Hamza <wachdarorihadchi@protonmail.ch>
2023-08-23 18:25:09 +0100

    solved problem 29

--- problem28.c
+++ problem28.c
@@ -6,8 +6,6 @@ int main(void){
 
 	// the max square size
 	uint32_t target = 1001;
-	// the current square size
-	uint64_t sqrsize = 2;
 	// the spiral sum
 	uint32_t sum = 1;
 	// last summed number
@@ -17,7 +15,6 @@ int main(void){
 		for(uint32_t j = 0; j != 4; j++){
 			prevnum += i;
 			sum += prevnum;
-			//printf("num: %u sum: %lu\n", prevnum, sum);
 		}
 	}
 
